8fa616adba35554012b3de953de503c53b41b292
JonatasCosta1983 <contactworkbetter@gmail.com>
2020-02-05 10:30:33 -0300

    Criado o J3, J2 e J1

--- tabeladeperto_app/lib/Tabela.dart
+++ tabeladeperto_app/lib/Tabela.dart
@@ -22,13 +22,13 @@ class _TabelaState extends State<Tabela> {
               ),
               ),
               Container(
-            width: 250.0,
+            width: 230.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text("Lentes multifocais\n"
-                     "de alta qualidade \n"
-                     "ao alcance de todos.\n",
+                Text("C  O  H  Z  V  L  G\n"
+                     "S  Z  N  D  C  J  X \n",
+                     //"ao alcance de todos.\n",
               style: TextStyle(fontFamily: ('Varela'),
               fontSize: 21.0
               ),
@@ -49,24 +49,24 @@ class _TabelaState extends State<Tabela> {
            children: <Widget>[
               Text('1,00m',
                style: TextStyle(fontFamily: ('Varela'),
-              fontSize: 17.0
+              fontSize: 21.0
               ),
               ),
               Container(
-            width: 250.0,
+            width: 230.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text("As lentes fotossensíveis \n"
-                     "protegem seus olhos \n"
-                     "contra os raios solares.\n",
+                Text("  K  C  N  R  D  B  S \n"
+                     "  V  O  Y  W  S  J  L  \n",
+                     //"contra os raios solares.\n",
                       style: TextStyle(fontFamily: ('Varela'),
               fontSize: 17.0
               ),
               ),
                 Text('J5',
                  style: TextStyle(fontFamily: ('Varela'),
-              fontSize: 17.0
+              fontSize: 21.0
               ),
               )
               ],
@@ -80,24 +80,23 @@ class _TabelaState extends State<Tabela> {
            children: <Widget>[
               Text('0,75m',
               style: TextStyle(fontFamily: ('Varela'),
-              fontSize: 16.0
+              fontSize: 21.0
               ),
               ),
               Container(
-            width: 250.0,
+            width: 230.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text("Lentes multifocais\n"
-                     "de alta qualidade \n"
-                     "ao alcance de todos.\n",
-                     style: TextStyle(fontFamily: ('Varela'),
-                    fontSize: 16.0
+                Text("     M  N  Q  O  W  M\n"
+                     "     L  D  S  A  N  Z  X \n",
+                    style: TextStyle(fontFamily: ('Varela'),
+                    fontSize: 13.0
               ),
                      ),
                 Text('J4',
               style: TextStyle(fontFamily: ('Varela'),
-              fontSize: 16.0
+              fontSize: 21.0
               ),
                 )
               ],
@@ -105,52 +104,91 @@ class _TabelaState extends State<Tabela> {
           )
          ],
         ),
-         Padding(padding: EdgeInsets.only(left: 15.0, top: 10.0, right: 15.0 )),
+         Padding(padding: EdgeInsets.only(left: 15.0, top: 20.0, right: 15.0 )),
          Row(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            children: <Widget>[
-              Text('0,62m'),
+              Text('0,62m',
+                style: TextStyle(fontFamily: ('Varela'),
+              fontSize: 21.0
+              ),
+              ),
               Container(
-            width: 140.0,
+            width: 230.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text('Carl Zeiss'),
-                Text('J3')
+                Text("       P  B  M  N  Q  O  W  M  \n"
+                     "          F  C  L  D  S  A  N  Z \n",
+                     style: TextStyle(fontFamily: ('Varela'),
+                    fontSize: 10.0
+                  ),
+                  ),
+                Text('J3',
+                  style: TextStyle(fontFamily: ('Varela'),
+              fontSize: 21.0
+              ),
+                )
               ],
             ),
           )
          ],
         ),
-         Padding(padding: EdgeInsets.only(left: 15.0, top: 10.0, right: 15.0 )),
+         Padding(padding: EdgeInsets.only(left: 15.0, top: 30.0, right: 15.0 )),
          Row(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            children: <Widget>[
-              Text('0,50m'),
+              Text('0,50m',
+               style: TextStyle(fontFamily: ('Varela'),
+              fontSize: 21.0
+              ),
+              ),
               Container(
-            width: 140.0,
+            width: 230.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text('Carl Zeiss'),
-                Text('J2')
+                Text("          P  B  M  N  Q  O  W  M\n"
+                     "          F  C  L  D  S  A  N  Z  X \n",
+                     style: TextStyle(fontFamily: ('Varela'),
+                    fontSize: 9.0
+                  ),
+                ),
+                Text('J2',
+                style: TextStyle(fontFamily: ('Varela'),
+              fontSize: 21.0
+              ),
+                )
               ],
             ),
           )
          ],
         ),
-         Padding(padding: EdgeInsets.only(left: 15.0, top: 10.0, right: 15.0 )),
+         Padding(padding: EdgeInsets.only(left: 15.0, top: 40.0, right: 15.0 )),
          Row(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            children: <Widget>[
-              Text('0,37m'),
+              Text('0,37m',
+              style: TextStyle(fontFamily: ('Varela'),
+              fontSize: 21.0
+              ),
+              ),
               Container(
-            width: 140.0,
+            width: 230.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text('Carl Zeiss'),
-                Text('J1')
+                Text("              P  B  M  N  Q  O  W  M\n"
+                     "              F  C  L  D  S  A  N  Z  X \n",
+                     style: TextStyle(fontFamily: ('Varela'),
+                    fontSize: 8.0
+                  ),
+                ),
+                Text('J1',
+                 style: TextStyle(fontFamily: ('Varela'),
+              fontSize: 21.0
+              ),
+                )
               ],
             ),
           )
