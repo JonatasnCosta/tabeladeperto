8e0f9dfc459bb80ee32df9dfae8fcfb5f470fb96
JonatasCosta1983 <contactworkbetter@gmail.com>
2022-05-19 12:17:52 -0300

    Criado diretorio PdfView

--- tabeladeperto_app/lib/Receita/ReceitaExpress.dart
+++ tabeladeperto_app/lib/Receita/ReceitaExpress.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:printing/printing.dart';
 import 'package:tabeladeperto_app/Home.dart';
-import 'package:tabeladeperto_app/PdfPreviewScreen.dart';
+import 'package:tabeladeperto_app/PdfView/PdfPreviewScreen.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:tabeladeperto_app/Receita/Checkbox/ArCheckBoxController.dart';
