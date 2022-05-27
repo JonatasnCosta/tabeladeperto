1658d458a83e18c71b1ac160d91b819d17829251
JonatasCosta1983 <contactworkbetter@gmail.com>
2022-05-27 15:32:10 -0300

    Modificado o nome home para homePageFree

---
+++ tabeladeperto_app/lib/AppWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tabeladeperto_app/HomePagerFree.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
--- tabeladeperto_app/lib/main.dart
+++ tabeladeperto_app/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:tabeladeperto_app/Home.dart';
+import 'package:tabeladeperto_app/AppWidget.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
-    //Criado o ponto de restauração para integrar o firebase
-    //Criado o ponto de restauração para integrar o firebase
+    home: AppWidget(),
   ));
 }
+
+
+//Criado o ponto de restauração para integrar o firebase
 //Ponto de intermédio para receber o firebase
 //Criado o ponto de restauração para integrar o firebase
 //Criado o ponto para integrar firebase
