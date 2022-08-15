1b86576cce197fd2675b68cd89a0889e53b4c36d
JonatasCosta1983 <contactworkbetter@gmail.com>
2022-08-15 14:35:51 -0300

    Adicionei um monte de imagens

--- tabeladeperto_app/lib/bemvindo_page.dart
+++ tabeladeperto_app/lib/bemvindo_page.dart
@@ -21,11 +21,49 @@ class _BemvindoPageState extends State<BemvindoPage> {
             width: double.maxFinite,
             height: double.maxFinite,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                      "assets/" + images[index],
-                    ),
-                    fit: BoxFit.cover)),
+              image: DecorationImage(
+                  image: AssetImage(
+                    "assets/" + images[index],
+                  ),
+                  fit: BoxFit.cover),
+            ),
+            child: Container(
+              margin: EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Ambiente',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'De ferramentas grátis',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 30,
+                        ),
+                      ),
+                      SizedBox(height: 20.00),
+                      Container(
+                        width: 250,
+                        child: Text(
+                          'Tanha na palma das suas mãos tabelas, testes para agilizar o seu dia a dia de refração.',
+                          style: TextStyle(
+                            color: Color(0xff878593),
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           );
         },
       ),
